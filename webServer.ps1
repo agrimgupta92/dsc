1128067430f8c3cb1325cba9230ceb106faa5d3d
AGRIM GUPTA <agrimgupta92@gmail.com>
2016-08-11 22:17:30 -0700

    update

--- webServer.ps1
+++ webServer.ps1
@@ -8,7 +8,7 @@ Configuration MyService
         Ensure = "Present"  
         Type = "Directory" 
         Recurse = $true 
-        SourcePath = $settingspath
+        SourcePath = "$settingspath"
         DestinationPath = "C:\HelloWorldPackage" 
 	}
 }
